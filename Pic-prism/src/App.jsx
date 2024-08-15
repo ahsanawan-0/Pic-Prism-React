1464e2baed29d040c10f4db35283a89b27d003dd
ahsanawan-0 <ahsanawan0482@gmail.com>
2024-08-15 15:17:42 +0500

    routing-setup

---
+++ Pic-prism/src/App.jsx
@@ -0,0 +1,24 @@
+import {BrowserRouter,Route,Routes} from "react-router-dom"
+import { Home } from "./pages/Home"
+import { Login } from "./pages/Login"
+import { Singup } from "./pages/Singup"
+import { SellerDashbod } from "./pages/SellerDashbod"
+import BuyerDashbod from "./pages/BuyerDashbod"
+import Navbar from "./components/Navbar"
+ export default function App() {
+  return (
+<>
+<BrowserRouter>
+<Navbar/>
+
+<Routes>
+  <Route path="/" element={<Home/>}/>
+  <Route path="/login" element={<Login/>}/>
+  <Route path="/singup" element={<Singup/>}/>
+  <Route path="/seller/profile" element={<SellerDashbod/>}/>
+  <Route path="/buyer/profile" element={<BuyerDashbod/>}/>
+</Routes>
+</BrowserRouter>
+</>
+  )
+}
---
+++ Pic-prism/src/pages/BuyerDashbod.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BuyerDashbod = () => {
+  return (
+    <div>
+      buyerDashboard 
+      
+    </div>
+  )
+}
+
+export default BuyerDashbod
